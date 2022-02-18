eb264c0d57e70fc755b8f596493304dc0b8560c5
Lauren Ferrett <93666073+LFerrett@users.noreply.github.com>
2022-02-18 13:53:26 -0500

    Added first graphic showcase section.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 var instance = M.Carousel.init({
-  fullWidth: true
+  fullWidth: true,
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.carousel');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
   var instances = M.Carousel.init(elems, options);
 });
 
-$(document).ready(function(){
-  $('.modal').modal();
+$(document).ready(function () {
+  $(".modal").modal();
 });
+
+var mb = document.querySelectorAll(".materialboxed");
+M.Materialbox.init(mb, {});
